0aa30133c5ac0b68e0e3ee72816e8b61fc763da2
KorenPapazian <kpapazian@edenschool.fr>
2022-06-17 14:22:26 +0200

    JS

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
-const pseudo = "mel";
-const img ='img/User_Avatar_2.png';
+const pseudo = "Gerard";
+const sponso = "sponsored";
+const points = "...";
+const likes = "250,223 likes";
+
+const avatar = "img/User_Avatar_2.png";
+const imgInsta = "img/instagram.webp";
+const imgHearth = "img/heart-solid.svg";
+const imgComment = "img/comment-solid.svg";
+const imgPlane = "img/paper-plane-solid.svg";
+const imgBookmark = "img/bookmark-solid.svg";
 
 
 document.body.innerHTML=
 `<article>
     <div id="header">
-        <img src="${img}">
+        <img src="${avatar}">
         <div>
             <p>${pseudo}</p>
-            <p>sponsored</p>
-            <p>...</p>
+            <p>${sponso}</p>
+            <p>${points}</p>
         </div>
     </div>
 
     <div id="main">
-        <img src="img/instagram.webp">
+        <img src="${imgInsta}">
     </div>
     
     <div id="footer">
         <div id="likeAndImg">
             <div id="gauche">
-                <img src="img/heart-solid.svg">
-                <img src="img/comment-solid.svg">
-                <img src="img/paper-plane-solid.svg">
+                <img src="${imgHearth}">
+                <img src="${imgComment}">
+                <img src="${imgPlane}">
             </div>
             <div id="likes">
-                <p>250,223 likes</p>
+                <p>${likes}</p>
             </div>
         </div>
             <div id="droite">
-                <img src="img/bookmark-solid.svg">
+                <img src="${imgBookmark}">
             </div>
     </div>
 </article>`;
